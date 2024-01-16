9b706f20c74eda3fdcf1a6fc28d86c47724d224b
Marr <marat.salah1313@gmail.com>
2024-01-16 18:59:04 +0100

    update API

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,17 @@ import fs from 'node:fs';
 
 const { promises: fsp } = fs;
 
-const app = express();
+export const app = express();
 const port = 3000;
+
+export const HTTP_STATUSES = {
+  OK_200: 200,
+  CREATED_201: 201,
+  NO_CONTENT_204: 204,
+
+  BAD_REQUEST_400: 400,
+  NOT_FOUND_404: 404,
+};
 
 const jsonBodyMiddleware = express.json();
 app.use(jsonBodyMiddleware);
@@ -38,9 +47,26 @@ app.get('/pages', (req, res) => {
   res.json(foundPages);
 });
 
+app.get('/pages/:id', (req, res) => {
+  const foundPage = db.pages.find((item) => item.id === +req.params.id);
+
+  if (!foundPage) {
+    res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
+    return;
+  }
+
+  res.json(foundPage);
+});
+
 app.post('/pages', (req, res) => {
-  if (!req.body.title) {
-    res.sendStatus(400);
+  const titleSpacesLength = req.body.title
+    .split('')
+    .filter((char: string) => char === ' ');
+  // check string with spaces only
+  const checkSpaces = req.body.title.length === titleSpacesLength;
+
+  if (!req.body.title || req.body.title === '' || checkSpaces) {
+    res.sendStatus(HTTP_STATUSES.BAD_REQUEST_400);
     return;
   }
 
@@ -52,18 +78,41 @@ app.post('/pages', (req, res) => {
 
   db.pages.push(createdPage);
 
-  res.json(createdPage);
+  res.status(HTTP_STATUSES.CREATED_201).json(createdPage);
 });
 
-app.get('/pages/:id', (req, res) => {
-  const foundPage = db.pages.find((item) => item.id === +req.params.id);
+app.delete('/pages/:id', (req, res) => {
+  const lengthDbPages = db.pages.length;
+  db.pages = db.pages.filter((page) => page.id !== +req.params.id);
 
-  if (!foundPage) {
-    res.sendStatus(404);
+  if (lengthDbPages === db.pages.length) {
+    res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
     return;
   }
 
-  res.json(foundPage);
+  res.sendStatus(HTTP_STATUSES.NO_CONTENT_204);
+});
+
+app.put('/pages/:id', (req, res) => {
+  if (!req.body.title) {
+    res.sendStatus(HTTP_STATUSES.BAD_REQUEST_400);
+    return;
+  }
+
+  const foundPage = db.pages.find((item) => item.id === +req.params.id);
+  if (!foundPage) {
+    res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
+    return;
+  }
+
+  foundPage.title = req.body.title;
+
+  res.sendStatus(HTTP_STATUSES.NO_CONTENT_204);
+});
+
+app.delete('/__test__/data', (req, res) => {
+  db.pages = [];
+  res.sendStatus(201);
 });
 
 app.listen(port, () => {
